445d0517c2bff1fedc590ada518207983564e636
Khanya Keswa <khanya.keswa@gmail.com>
2022-11-08 09:31:26 -0600

    fetch delete request troubleshooting
    Co-authored-by: ajdavid128 <ajdavid128@users.noreply.github.com>

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,40 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import { React, useEffect, useState } from 'react'
+import ListingCard from './ListingCard'
 
 function ListingsContainer() {
+  const [listingState, setListings] = useState([])
+
+  const handleDelete = (id) => {
+    // fetch(`http://localhost:6001/listings/${id}`, { method: 'DELETE' })
+    // .then(response => response.json())
+    console.log(id)
+  }
+
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+      .then((response) => response.json())
+      .then((listingsData) => setListings(listingsData))
+  }, [])
+  
+  const listingsArray = listingState.map((listing) => (
+    <ListingCard
+      key={listing.id}
+      description={listing.description}
+      image={listing.image}
+      location={listing.location}
+      id={listing.id}
+      handleDelete={handleDelete}
+    />
+  ))
+
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+      <ul className='cards'>
+        {listingsArray}
       </ul>
     </main>
-  );
+  )
 }
 
-export default ListingsContainer;
+export default ListingsContainer
